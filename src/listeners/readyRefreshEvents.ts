cda570eabdee35236e764794189638d66c96d7cd
Phil <philip.schaefer0@gmail.com>
2025-11-11 00:56:40 +0100

    chore: added clarification logging message

--- src/listeners/readyRefreshEvents.ts
+++ src/listeners/readyRefreshEvents.ts
@@ -16,7 +16,7 @@ export class ReadyListener extends Listener<typeof Events.ClientReady> {
   }
 
   public run(client: Client) {
-    if (process.env.NODE_ENV === 'development') return;
+    if (process.env.NODE_ENV === 'development') return this.container.logger.debug('Did not run initial refreshScheduledEvents in debug.');
     this.container.logger.info('Updating event tables!');
     config.discordEventMgmt.managedGuilds.forEach(async (mgmt) => {
       const guild = await client.guilds.fetch(mgmt.guildId);
