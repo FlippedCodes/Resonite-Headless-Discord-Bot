198a2c138b1eb516680f91c5cb5d1a3fb611b73a
Phil <philip.schaefer0@gmail.com>
2026-01-05 22:55:01 +0100

    fix: removed temporary code

--- src/lib/discordGetResoniteWorldList.ts
+++ src/lib/discordGetResoniteWorldList.ts
@@ -10,9 +10,7 @@ import type { container as containerType } from '../types';
 
 // function to get information about one session.
 export async function getWorlds(dockerContainer: containerType, interaction?: CommandInteraction) {
-  // TEMP: couldn't restart the container due to ongoing events and i was impatient to try prod.
-  // const channelId = dockerContainer.Labels.discordBotWorldListChannel;
-  const channelId = '1040278418173993070';
+  const channelId = dockerContainer.Labels.discordBotWorldListChannel;
   if (!channelId) return { successful: false, response: 'No channel for a world list was set.' };
   // const guildMember = interaction.member as GuildMember;
   // FIXME: fetch is not working and never returns
